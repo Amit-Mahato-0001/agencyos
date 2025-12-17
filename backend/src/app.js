674674105aad1661dfc6435ed23308459bcd2685
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2025-12-17 18:03:24 +0530

    cors setup and health check

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 const connectDB = require("./config/db");
-
-connectDB();
-
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req,res) => {
+    res.json({ status: "OK" });
+})
 
 module.exports = app;
